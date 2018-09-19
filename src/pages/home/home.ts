77f46215ce899c8427221b75190cf7cc974c68b9
João Mesquita <juaomesquita@gmail.com>
2018-09-19 00:11:48 -0300

    Atualização, pegando o destino

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,17 +2,27 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { PerguntasPage } from '../perguntas/perguntas';
 
+import { AngularFireDatabase } from 'angularfire2/database'
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  arrData = []
+  destino
 
+  constructor(public navCtrl: NavController, private fdb: AngularFireDatabase) {
+    this.fdb.list("/meusDados/").subscribe(_data => {
+      this.arrData = _data;
+
+      console.log(this.arrData)
+    })
   }
 
   perguntas() {
+    this.fdb.list("/meusDados/perguntas/destino").push(this.destino)
     this.navCtrl.push(PerguntasPage);
   }
 
