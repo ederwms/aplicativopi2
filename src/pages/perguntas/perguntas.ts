7d417fa8e31315c53c8ecfbaec77e494d120c011
João Mesquita <juaomesquita@gmail.com>
2018-09-22 23:51:28 -0300

    Slides mudando ao clicar na resposta

--- src/pages/perguntas/perguntas.ts
+++ src/pages/perguntas/perguntas.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ListaPage } from '../lista/lista';
 import { AngularFireDatabase } from 'angularfire2/database';
+import { ViewChild } from '@angular/core';
+import { Slides } from 'ionic-angular';
+
 
 @IonicPage()
 @Component({
@@ -13,6 +16,8 @@ export class PerguntasPage {
   arrData = []
   resposta: any;
   private path: string = "meusDados/";
+  @ViewChild(Slides) slides: Slides;
+
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private fdb: AngularFireDatabase) {
   
@@ -27,14 +32,24 @@ export class PerguntasPage {
   respostaSim() {
     this.resposta = 'sim'
     this.fdb.list(this.path + "respostas/").push(this.resposta)
+    this.slides.lockSwipes(false)
+    this.slides.slideNext(500)
+    this.slides.lockSwipes(true)
   }
 
   respostaNao() {
     this.resposta = 'nao'
     this.fdb.list(this.path + "respostas/").push(this.resposta)
+    this.slides.lockSwipes(false)
+    this.slides.slideNext(500)
+    this.slides.lockSwipes(true)
   }
 
   goLista() {
     this.navCtrl.push(ListaPage);
+  }
+
+  ionViewDidLoad(){
+    this.slides.lockSwipes(true)
   }
 }
