8eb99da8496cac3dfa859e71eba296a60c7b793a
murilofm00 <38534607+murilofm00@users.noreply.github.com>
2018-10-28 18:58:34 -0300

    Adicionar itens manualmente

--- src/pages/lista/lista.ts
+++ src/pages/lista/lista.ts
@@ -14,6 +14,7 @@ export class ListaPage {
   path: any = "meusDados/"
   busca: any;
   userId: string = firebase.auth().currentUser.uid;
+  newItem: string;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.pegaDados();
@@ -49,10 +50,29 @@ export class ListaPage {
 
   escreveLista() {
     firebase.database().ref('meusDados/respostas/' + this.userId).child('lista').on('child_added', function(snap){
-      const li = document.createElement("li")
+      const li = document.createElement("li");
       li.innerText = snap.val()
+      li.id = snap.key;
       document.getElementById("lista").appendChild(li);
     })
+
+    firebase.database().ref('meusDados/respostas/' + this.userId).child('lista').on('child_changed', function(snap){
+      const liAlterou = document.getElementById(snap.key)
+      liAlterou.innerText = snap.val()
+    })
+    firebase.database().ref('meusDados/respostas/' + this.userId).child('lista').on('child_removed', function(snap){
+      const liRemovida = document.getElementById(snap.key)
+      liRemovida.remove();
+    })
+  }
+
+  adicionarItem() {
+    var novoItemKey = firebase.database().ref('meusDados/respostas/' + this.userId).child('lista').push().key;
+    firebase.database().ref('meusDados/respostas/' + this.userId + '/lista').update({
+      [novoItemKey]: this.newItem,
+    })
+    console.log(this.newItem)
+    this.newItem = null
   }
 
 }
