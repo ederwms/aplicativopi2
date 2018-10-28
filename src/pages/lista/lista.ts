1f4cfac67e23c66f1a13702feb387b0390106ab9
murilofm00 <38534607+murilofm00@users.noreply.github.com>
2018-10-28 13:10:39 -0300

    Lista

--- src/pages/lista/lista.ts
+++ src/pages/lista/lista.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import firebase from 'firebase';
+import { createElement } from '@angular/core/src/view/element';
 
 @IonicPage()
 @Component({
@@ -12,20 +13,34 @@ export class ListaPage {
   teste: any;
   path: any = "meusDados/"
   busca: any;
+  userId: string = firebase.auth().currentUser.uid;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.pegaDados();
+    this.escreveLista();
   }
 
   pegaDados() {
-    let banco = firebase.database().ref('meusDados/respostas');
-    banco.once('value', function(result) {
-      result.forEach(function(resultado) {
-        let childKey = resultado.key;
-        let childValue = resultado.val();
-        console.log("Key" + childKey);
-        console.log("Valor: " + childValue);
-      })
+    console.log(this.userId)
+    var proBanco = firebase.database().ref('meusDados/respostas/' + this.userId + '/lista')
+    firebase.database().ref('meusDados/respostas/' + this.userId).once('value', function(snap){
+      var pergunta = snap.val()
+      if (pergunta.esportes == 'sim')
+        proBanco.update({esportes: 'Equipamentos esportivos'})
+      if (pergunta.parqueAq == 'sim')
+        proBanco.update({parqueAq:'Roupa de banho'})
+      if (pergunta.passeioN == 'sim')
+        proBanco.update({passeioN:'Roupa de passeio'})
+      if (pergunta.trabalho == 'sim')
+        proBanco.update({trabalho:'Roupa social'})
+    })
+  }
+
+  escreveLista() {
+    firebase.database().ref('meusDados/respostas/' + this.userId).child('lista').on('child_added', function(snap){
+      const li = document.createElement("li")
+      li.innerText = snap.val()
+      document.getElementById("lista").appendChild(li);
     })
   }
 
