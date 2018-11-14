283a54427b603b65b4b53702a73555fce8533f8d
Éder Sousa <ederwillian1880@gmail.com>
2018-11-14 11:53:59 -0200

    Correção bugs API Clima, começo de zoeira com cidades na lista

--- src/pages/lista/lista.ts
+++ src/pages/lista/lista.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 import firebase from 'firebase';
 import { createElement } from '@angular/core/src/view/element';
 import { HttpClient } from '@angular/common/http';
@@ -11,17 +11,15 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ListaPage {
 
-  // Chaves das API
-  forecastKey: any = 'd6930b3aaccb1375812ce727ea86dbfc';
-  geocodingKey: any = 'ZPPzipVJfjQixqkURm75wzqnAnVZzRL9';
-  crossOrigin = 'https://cors-anywhere.herokuapp.com/';
-  // Variavel que armazena o objeto que contem as coordenadas. Mais facil de manipular o objeto com essa variavel.
+  // Variavel que armazena o objeto que contem as coordenadas.
+  // É mais facil de manipular o objeto com essa variavel.
   coordenadas;
   // Variavel que armazena o objeto da previsao do tempo.
   previsao;
   // Variaveis input
   myDate: any;
   cidade: any;
+  horario: any = new Date();
 
 
   // --------------------------------------------------------------------------------------------------
@@ -31,23 +29,30 @@ export class ListaPage {
   userId: string = firebase.auth().currentUser.uid;
   newItem: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public http: HttpClient) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: HttpClient, public loadingCtrl: LoadingController) {
     this.myDate = localStorage.getItem('data');
     this.cidade = localStorage.getItem('destino');
 
-    this.buscaClima(this.cidade, this.myDate);
+    //this.buscaClima(this.cidade, this.myDate);
     this.pegaDados();
     this.escreveLista();
   }
 
   // Método para buscar os dados de clima com as APIs Geocoding(para coordenadas) e DarkSky Weather API 
   buscaClima(destino: string, data: any) {
-    return this.http.get('http://www.mapquestapi.com/geocoding/v1/address?key=' + this.geocodingKey + '&location=' + destino + '&maxResults=1').subscribe(geo => {
+    // Requisição a MapQuest API para obter as coordenadas da cidade digitada.
+    return this.http.get('http://www.mapquestapi.com/geocoding/v1/address?key=ZPPzipVJfjQixqkURm75wzqnAnVZzRL9&location='+destino+'&maxResults=1').subscribe(geo => {
       this.coordenadas = geo;
       let lat = this.coordenadas.results['0'].locations['0'].latLng.lat; // Latitude
       let long = this.coordenadas.results['0'].locations['0'].latLng.lng; // Longitude
 
-      let url = this.crossOrigin + 'https://api.darksky.net/forecast/' + this.forecastKey + '/' + lat + ',' + long + ',' + data + 'T00:00:00?units=si&lang=pt&exclude=minutely,flags&outFormat=json';
+      // Variaveis que pegam  o tempo do sistema em HH:MM:SS.
+      let horas = this.horario.getHours() < 10 ? '0' + this.horario.getHours() : this.horario.getHours();
+      let minutos = this.horario.getMinutes() < 10 ? '0' + this.horario.getMinutes() : this.horario.getMinutes();
+      let segundos = this.horario.getSeconds() < 10 ? '0' + this.horario.getSeconds() : this.horario.getSeconds();
+
+      // Requisição a DarkSky API
+      let url = 'https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/d6930b3aaccb1375812ce727ea86dbfc/'+lat+','+long+','+data+'T'+horas+':'+minutos+':'+segundos+'?units=si&lang=pt&exclude=minutely,flags&outFormat=json';
       this.http.get(url).subscribe(data => {
         this.previsao = data;
         let info = document.querySelector('ion-content h1.rj');
@@ -59,11 +64,50 @@ export class ListaPage {
   }
 
   pegaDados() {
+    let cidadeDestino = this.cidade;
     firebase.database().ref('meusDados/respostas/' + this.userId + '/lista').set({ esportes: null })
     var novoItemKey = firebase.database().ref('meusDados/respostas/' + this.userId).child('lista');
     var proBanco = firebase.database().ref('meusDados/respostas/' + this.userId + '/lista')
     firebase.database().ref('meusDados/respostas/' + this.userId).once('value', function (snap) {
-      var pergunta = snap.val()
+      var pergunta = snap.val();
+      // Zoeira das cidades
+      if (cidadeDestino == 'Rio de Janeiro') {
+        proBanco.update({
+          [novoItemKey.push().key]: 'Colete a prova de balas',
+          [novoItemKey.push().key]: 'AK-47',
+          [novoItemKey.push().key]: 'Item zoando a cidade'
+        })
+      }
+      else if (cidadeDestino == 'Belo Horizonte') {
+        proBanco.update({
+          [novoItemKey.push().key]: 'Pão de Queijo', // Mudaremos essa depois, pão de queijo fica meio ruim kkkk
+        })
+      }
+      else if (cidadeDestino == 'Goiânia') {
+        proBanco.update({
+          [novoItemKey.push().key]: 'Berrante',
+          [novoItemKey.push().key]: 'Item zoando a cidade'
+        })
+      }
+      else if (cidadeDestino == 'Gramado') {
+        proBanco.update({
+          [novoItemKey.push().key]: 'Muitos agasalhos',
+          [novoItemKey.push().key]: 'Item zoando a cidade'
+        })
+      }
+      else if (cidadeDestino == 'Salvador') {
+        proBanco.update({
+          [novoItemKey.push().key]: 'Berimbau',
+          [novoItemKey.push().key]: 'Item zoando a cidade'
+        })
+      }
+      else if (cidadeDestino == 'São Paulo') {
+        proBanco.update({
+          [novoItemKey.push().key]: 'Item zoando a cidade'
+        })
+      }
+
+      // Lista Séria
       if (pergunta.esportes == 'sim') {
         proBanco.update({
           [novoItemKey.push().key]: 'Camisa regata',
@@ -82,12 +126,11 @@ export class ListaPage {
 
 
       if (pergunta.passeioN == 'sim') {
-        proBanco.update({
-          [novoItemKey.push().key]: 'Roupa de passeio',
-          [novoItemKey.push().key]: 'Câmera fotográfica',
-          [novoItemKey.push().key]: 'Colete a prova de balas',
-          [novoItemKey.push().key]: 'AK-47'
-        })
+          proBanco.update({
+            [novoItemKey.push().key]: 'Roupa de passeio',
+            [novoItemKey.push().key]: 'Câmera fotográfica',
+            [novoItemKey.push().key]: 'Outro item de Turista',
+          })
       }
 
 
@@ -99,7 +142,30 @@ export class ListaPage {
     })
   }
 
-  escreveLista() {
+  async delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
+  }
+
+  presentLoadingDefault() {
+    let loading = this.loadingCtrl.create({
+      content: 'Aguarde, estamos montando sua lista...'
+    });
+  
+    loading.present();
+  
+    setTimeout(() => {
+      loading.dismiss();
+    }, 5000);
+  }
+
+  async escreveLista() {
+    // Função para previsão de clima
+    this.buscaClima(this.cidade, this.myDate);
+    // As duas funções abaixo fazem o efeito de espera para mostrar a lista.
+    // Delay de 5s
+    this.presentLoadingDefault();
+    await this.delay(5000);
+    // Mostra a lista na tela
     firebase.database().ref('meusDados/respostas/' + this.userId).child('lista').on('child_added', function (snap) {
       var itemId = snap.key + "Item";
       const li = document.createElement("ion-label");
