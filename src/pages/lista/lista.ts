041e88442288cfb2fc6fb397384cbd1b2a95469a
Brenda <Brebsp@users.noreply.github.com>
2018-11-19 21:57:13 -0200

    Revert "lista ifs"
    This reverts commit 60126475e8f9718076ca009cf13a78c959325fa2.

--- src/pages/lista/lista.ts
+++ src/pages/lista/lista.ts
@@ -108,29 +108,11 @@ export class ListaPage {
       }
 
       // Lista Séria
-      /*if (pergunta.esportes == 'sim') {
-        
+      if (pergunta.esportes == 'sim') {
+        proBanco.update({
           [novoItemKey.push().key]: 'Camisa regata',
           [novoItemKey.push().key]: 'Shorts',
           [novoItemKey.push().key]: 'Tênis para esportes',
-        })
-      }*/
-      if (pergunta.esportes.includes('fut')) {
-        proBanco.update({
-        [novoItemKey.push().key]: 'Bola de futebol',
-        [novoItemKey.push().key]: 'Chuteira',
-        })
-      }
-      if (pergunta.esportes.includes('bas')) {
-        proBanco.update({
-        [novoItemKey.push().key]: 'Bola de basquete',
-        [novoItemKey.push().key]: 'Tênis',
-          
-        })
-      }
-      if (pergunta.esportes.includes('ten')) {
-        proBanco.update({
-        [novoItemKey.push().key]: 'Raquete de tenis'
         })
       }
 
